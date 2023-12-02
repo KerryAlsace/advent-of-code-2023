623e35e680bdbfed7ed65a6f8539c3d460095d14
KerryAlsace <kerrydougherty@gmail.com>
2023-12-02 12:48:31 -0500

    save

--- day_one/main.go
+++ day_one/main.go
@@ -7,14 +7,58 @@ import (
 	"strings"
 )
 
+var NumberStringsToInt = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  8,
+}
+
+var NumberFirstLetters = map[string][]string{
+	"o": {
+		"one",
+	},
+	"t": {
+		"two",
+		"three",
+	},
+	"f": {
+		"four",
+		"five",
+	},
+	"s": {
+		"six",
+		"seven",
+	},
+	"e": {
+		"eight",
+	},
+	"n": {
+		"nine",
+	},
+}
+
 func main() {
 	i := getInput()
-	fmt.Printf("Part One answer is: %v\n", partOne(i))
-	fmt.Printf("Part Two answer is: %v\n", partTwo(i))
+	partOneAns, err := partOne(i)
+	if err != nil {
+		fmt.Printf("Error in partOne: %v\n", err)
+	}
+	partTwoAns, err := partTwo(i)
+	if err != nil {
+		fmt.Printf("Error in partTwo: %v\n", err)
+	}
+	fmt.Printf("Part One answer is: %v\n", partOneAns)
+	fmt.Printf("Part Two answer is: %v\n", partTwoAns)
 }
 
 func getInput() []string {
-	b, err := ioutil.ReadFile("input.txt")
+	b, err := ioutil.ReadFile("part_two_example_input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -22,39 +66,45 @@ func getInput() []string {
 	return strings.Split(string(b), "\n")
 }
 
-func partOne(input []string) int {
+func partOne(input []string) (int, error) {
 	totalSum := 0
 	for _, val := range input {
-		a := calculateCalibrationValue(val)
+		a, err := calculateCalibrationValue(val)
+		if err != nil {
+			return 0, fmt.Errorf("Error for val %v: %v", val, err)
+		}
 		totalSum += a
 	}
-	return totalSum
+	return totalSum, nil
 }
 
-func partTwo(input []string) int {
+func partTwo(input []string) (int, error) {
 	totalSum := 0
 	for _, val := range input {
-		a := calculateCalibrationValueWithStrings(val)
+		a, err := calculateCalibrationValueWithStrings(val)
+		if err != nil {
+			return 0, fmt.Errorf("Error for val %v: %v\n", val, err)
+		}
 		totalSum += a
 	}
-	return totalSum
+	return totalSum, nil
 }
 
-func calculateCalibrationValue(i string) int {
+func calculateCalibrationValue(i string) (int, error) {
 	f, err := findFirstNumber(i)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 	l, err := findLastNumber(i)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 	fmtdAns := fmt.Sprintf("%d%d", f, l)
 	n, err := strconv.Atoi(fmtdAns)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-	return n
+	return n, nil
 }
 
 func findFirstNumber(input string) (int, error) {
@@ -86,27 +136,100 @@ func findLastNumber(input string) (int, error) {
 	return 0, fmt.Errorf("no last number found")
 }
 
-func calculateCalibrationValueWithStrings(i string) int {
+func calculateCalibrationValueWithStrings(i string) (int, error) {
 	f, err := findFirstNumberWithStrings(i)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
+	fmt.Printf("findFirstNumberWithStrings is %v\n", f)
 	l, err := findLastNumberWithStrings(i)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
 	fmtdAns := fmt.Sprintf("%d%d", f, l)
 	n, err := strconv.Atoi(fmtdAns)
 	if err != nil {
-		panic(err)
+		return 0, err
 	}
-	return n
+	return n, nil
 }
 
 func findFirstNumberWithStrings(input string) (int, error) {
-	return 0, nil
+	for i, char := range input {
+		fmtdChar := fmt.Sprintf("%c", char)
+		// First check if it is an int, and return it if so
+		n, nilIfInt := strconv.Atoi(fmtdChar)
+		if nilIfInt != nil {
+			// fmtdChar is not an int, so check if it is the starting letter
+			// of one of the number strings
+			if NumberFirstLetters[fmtdChar] != nil {
+				n, err := checkForNumString(input, i, fmtdChar)
+				if err != nil {
+					continue
+				}
+				return n, nil
+			}
+
+			continue
+		}
+
+		return n, nil
+	}
+
+	return 0, fmt.Errorf("no first number found")
 }
 
 func findLastNumberWithStrings(input string) (int, error) {
-	return 0, nil
+	lastIndex := len(input) - 1
+	for i := lastIndex; i >= 0; i-- {
+		fmt.Printf("i is %v\n", i)
+		fmtdChar := fmt.Sprintf("%c", input[i])
+		// First check if it is an int, and return it if so
+		n, nilIfInt := strconv.Atoi(fmtdChar)
+		if nilIfInt != nil {
+			// fmtdChar is not an int, so check if it is the starting letter
+			// of one of the number strings
+			if NumberFirstLetters[fmtdChar] != nil {
+				n, err := checkForNumString(input, i, fmtdChar)
+				if err != nil {
+					continue
+				}
+				return n, nil
+			}
+
+			continue
+		}
+
+		return n, nil
+	}
+	return 0, fmt.Errorf("no last number found")
+}
+
+func checkForNumString(input string, index int, s string) (int, error) {
+	possibleNumbers := NumberFirstLetters[s]
+	fmt.Printf("possibleNumbers: %v\n", possibleNumbers)
+	for _, numNameString := range possibleNumbers {
+		fmt.Printf("numNameString: %v\n", numNameString)
+		lenNumString := len(numNameString)
+		fmt.Printf("lenNumString: %v\n", lenNumString)
+		endIndex := lenNumString - index
+		fmt.Printf("endIndex: %v\n", endIndex)
+		inputEndIndex := endIndex + index
+		fmt.Printf("inputEndIndex: %v\n", inputEndIndex)
+		if inputEndIndex < index {
+			fmt.Printf("inputEndIndex %v < index %v\n", inputEndIndex, index)
+			continue
+		}
+		// Check that the input supports that last index
+		if len(input)-1 < inputEndIndex {
+			continue
+		}
+		fmt.Printf("input[index:inputEndIndex]: input[%v:%v]\n", index, inputEndIndex)
+		wordInInput := input[index:inputEndIndex]
+		fmt.Printf("wordInInput: %v\nnumNameString: %v", wordInInput, numNameString)
+		if wordInInput == numNameString {
+			return NumberStringsToInt[wordInInput], nil
+		}
+	}
+	return 0, fmt.Errorf("not found")
 }
--- day_one/main_test.go
+++ day_one/main_test.go
@@ -34,7 +34,11 @@ func TestCalculateCalibrationValue(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			if calculatedOutput := calculateCalibrationValue(tt.input); calculatedOutput != tt.expectedOutput {
+			calculatedOutput, err := calculateCalibrationValue(tt.input)
+			if err != nil {
+				t.Errorf("calculateCalibrationValue() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
 				t.Errorf("calculateCalibrationValue() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
 			}
 		})
@@ -61,7 +65,11 @@ func TestPartOne(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			if calculatedOutput := partOne(tt.input); calculatedOutput != tt.expectedOutput {
+			calculatedOutput, err := partOne(tt.input)
+			if err != nil {
+				t.Errorf("partOne() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
 				t.Errorf("partOne() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
 			}
 		})
@@ -91,8 +99,154 @@ func TestPartTwo(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			if calculatedOutput := partOne(tt.input); calculatedOutput != tt.expectedOutput {
-				t.Errorf("partOne() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
+			calculatedOutput, err := partTwo(tt.input)
+			if err != nil {
+				t.Errorf("partTwo() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
+				t.Errorf("partTwo() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
+			}
+		})
+	}
+}
+
+func TestCheckForNumString(t *testing.T) {
+	tests := []struct {
+		testName       string
+		input          string
+		index          int
+		s              string
+		expectedOutput int
+	}{
+		{
+			testName:       "1",
+			input:          "two1nine",
+			index:          0,
+			s:              "t",
+			expectedOutput: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			calculatedOutput, err := checkForNumString(tt.input, tt.index, tt.s)
+			if err != nil {
+				t.Errorf("checkForNumString() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
+				t.Errorf("checkForNumString() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
+			}
+		})
+	}
+}
+
+func TestCalculateCalibrationValueWithStrings(t *testing.T) {
+	tests := []struct {
+		testName       string
+		input          string
+		expectedOutput int
+	}{
+		{
+			testName:       "1",
+			input:          "two1nine",
+			expectedOutput: 29,
+		},
+		{
+			testName:       "2",
+			input:          "eightwothree",
+			expectedOutput: 83,
+		},
+		{
+			testName:       "3",
+			input:          "abcone2threexyz",
+			expectedOutput: 13,
+		},
+		{
+			testName:       "4",
+			input:          "xtwone3four",
+			expectedOutput: 24,
+		},
+		{
+			testName:       "5",
+			input:          "4nineeightseven2",
+			expectedOutput: 42,
+		},
+		{
+			testName:       "6",
+			input:          "zoneight234",
+			expectedOutput: 14,
+		},
+		{
+			testName:       "7",
+			input:          "7pqrstsixteen",
+			expectedOutput: 76,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			calculatedOutput, err := calculateCalibrationValueWithStrings(tt.input)
+			if err != nil {
+				t.Errorf("calculateCalibrationValueWithStrings() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
+				t.Errorf("calculateCalibrationValueWithStrings() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
+			}
+		})
+	}
+}
+
+func TestFindFirstNumberWithStrings(t *testing.T) {
+	tests := []struct {
+		testName       string
+		input          string
+		expectedOutput int
+	}{
+		{
+			testName:       "1",
+			input:          "two1nine",
+			expectedOutput: 2,
+		},
+		{
+			testName:       "2",
+			input:          "eightwothree",
+			expectedOutput: 8,
+		},
+		{
+			testName:       "3",
+			input:          "abcone2threexyz",
+			expectedOutput: 1,
+		},
+		{
+			testName:       "4",
+			input:          "xtwone3four",
+			expectedOutput: 2,
+		},
+		{
+			testName:       "5",
+			input:          "4nineeightseven2",
+			expectedOutput: 4,
+		},
+		{
+			testName:       "6",
+			input:          "zoneight234",
+			expectedOutput: 1,
+		},
+		{
+			testName:       "7",
+			input:          "7pqrstsixteen",
+			expectedOutput: 7,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			calculatedOutput, err := findFirstNumberWithStrings(tt.input)
+			if err != nil {
+				t.Errorf("findFirstNumberWithStrings() returned an error: %v", err)
+			}
+			if calculatedOutput != tt.expectedOutput {
+				t.Errorf("findFirstNumberWithStrings() = [%v], expected [%v]", calculatedOutput, tt.expectedOutput)
 			}
 		})
 	}
